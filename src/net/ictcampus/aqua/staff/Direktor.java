7104c2882ace782c7040f73ca746d25bca6c379d
Yaël Wehrli <wehrliy@ict-campus.net>
2020-04-16 11:47:10 +0200

    new Branch

---
+++ src/net/ictcampus/aqua/staff/Direktor.java
@@ -0,0 +1,24 @@
+public class Direktor {
+	private String name;
+	private int yearsOfService;
+
+	public void doThings() {
+		System.out.println("I'm working.");
+	}
+
+	public int getYearsOfService() {
+		returnyearsOfService;
+	}
+
+	public void setYearsOfService(int yearsOfService) {
+		this.yearsOfService= yearsOfService;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name= name;
+	}
+}
